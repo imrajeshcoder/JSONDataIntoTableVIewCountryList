aee14f8adcb45add649c1dd94e1cb55eb1d3a02c
Rajesh Shiyal <imrajesh.coder@gmail.com>
2021-02-05 10:41:22 +0530

    JSON Parsing done.

--- tableviewDisplayJSONDataIntoTableVIew/tableviewDisplayJSONDataIntoTableVIew/ViewController.swift
+++ tableviewDisplayJSONDataIntoTableVIew/tableviewDisplayJSONDataIntoTableVIew/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var arrayCountry = [[String: Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let path = Bundle.main.path(forResource: "county", ofType: "json") else { return }
+                let url = URL(fileURLWithPath: path)
+                
+                do {
+                    let data = try Data(contentsOf: url)
+                    let json = try JSONSerialization.jsonObject(with: data)as? [String: Any]
+                    guard let array = json?["country_info"] as? [[String: Any]]  else { return }
+                    self.arrayCountry = array
+                    print(self.arrayCountry[0]["country_name"])
+                
+                }
+                catch {
+                    print(error)
+                }
     }
-
-
+        
 }
 
